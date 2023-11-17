1e2e2cc4404d84c3f4062c0b69beed42fb7c8956
jtm <noreply@jtm.cx>
2023-11-17 14:04:54 -0500

    Move locale hack out of Justfile

--- flake.nix
+++ flake.nix
@@ -21,21 +21,17 @@
       ];
 
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
         haskellProjects.default = {
           devShell = {
             # By default, the haskell-flake module populates `devShells.default`
             # for us. While it's nice that haskell-flake has magically defined a
-            # devShell, it's difficult to customize and extend. To work around
-            # this, we "disable" the devShell here (i.e., don't let
-            # haskell-flake set `devShells.default`), and then define our own
-            # explicit `devShell` below, which derives its inputs from this one.
+            # devShell, it's difficult to extend. To work around this, we
+            # "disable" the devShell here (i.e., don't let haskell-flake set
+            # `devShells.default`), and then define our own explicit `devShell`
+            # below, which derives its inputs from this one.
             enable = false;
 
-            # Disable flake check to verify that HLS works.
+            # Disable flake check that verifies HLS is working.
             hlsCheck.enable = false;
           };
         };
@@ -46,14 +42,19 @@
           ];
           nativeBuildInputs = [
             pkgs.just
+
+            # There is an unusual locale issue when running GHC under Nix. If
+            # 'pkgs.glibcLocales' is not included as a 'nativeBuildInput', 'stt'
+            # will abort when it tries to print a unicode character with:
+            #
+            #    <stdout>: commitBuffer: invalid argument (invalid character)
+            #    <stdout>: hPutChar: invalid argument (invalid character)
+            #
+            # I still need to understand the root cause of this, and understand
+            # whether or not including glibcLocales is the "correct" solution.
+            pkgs.glibcLocales
           ];
         };
-      };
-
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
       };
     };
 }
